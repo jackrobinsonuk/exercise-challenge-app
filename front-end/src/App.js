9e0e083548377312ab9d0d3a86d8dbffad37fc37
Jack Robinson <jack.robinson@ibm.com>
2022-03-20 21:33:10 +0000

    fixed errors

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import NavBar from "./Components/NavBar";
 import Team from "./Pages/Team";
