045ed6838a4c2cb4e2add3fa40e529a2606e6292
Jack Robinson <jack.robinson@ibm.com>
2022-03-28 14:30:26 +0100

    added table to exercises page

---
+++ front-end/src/Components/ExerciseTable.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+export default function ExerciseTable(props) {
+  const exercises = props.exercises;
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Exercise ID</TableCell>
+            <TableCell>Exercise Name</TableCell>
+            <TableCell>Minutes</TableCell>
+            <TableCell>Date</TableCell>
+            <TableCell>Points</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {exercises.map((exercise) => (
+            <TableRow
+              key={exercise.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {exercise.id}
+              </TableCell>
+              <TableCell>{exercise.Name}</TableCell>
+              <TableCell>{exercise.Minutes}</TableCell>
+              <TableCell>{exercise.Date}</TableCell>
+              <TableCell>{exercise.Points}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- front-end/src/Pages/YourExercise.js
+++ front-end/src/Pages/YourExercise.js
@@ -1,10 +1,22 @@
 import { React, useState } from "react";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
+import { CircularProgress } from "@mui/material";
 import axios from "axios";
+
+import ExerciseTable from "../Components/ExerciseTable";
 
 export default function YourExercise() {
   const [exercises, setExercises] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [totalPoints, setTotalPoints] = useState(null)
+
+function calculateTotalPoints() {
+var totalPoints = exercises.reduce(function(prev, cur) {
+  return prev + cur.Points;
+}, 0)
+setTotalPoints(totalPoints)
+}
 
   function getMyExercises() {
     axios({
@@ -12,22 +24,39 @@ export default function YourExercise() {
       url: "https://mmhbb4sn0k.execute-api.eu-west-1.amazonaws.com/Prod/user/get-exercise/Jack",
       responseType: "json",
     }).then(function (response) {
-      setExercises(response.data.toString());
+      setExercises(response.data);
+      
+    }).then(function () {
+      setLoading(false);
+    }).then(function () {
+      calculateTotalPoints()
     });
   }
-  getMyExercises();
+  if (loading === true) {
+    getMyExercises();
+  }
   return (
-    <main style={{ paddingLeft: "20px" }}>
+    <main style={{ padding: "20px" }}>
       <h2>Your Exercise</h2>
-      Add a table here with exercises done by the user. {exercises}
-      <h3>Add Exercise</h3>
-      <Fab
-        color="primary"
-        aria-label="add"
-        onClick={() => console.log("Add Exercise Clicked")}
-      >
-        <AddIcon />
-      </Fab>
+      {loading === true && <div>
+        <CircularProgress />
+        <h4>Total Points: Loading</h4>
+        </div>}
+      
+      {loading === false && <div>
+        <ExerciseTable exercises={exercises} />
+        <h4>Total Points: {totalPoints}</h4>
+        </div>}
+      <div align="right">
+        <h3>Add Exercise</h3>
+        <Fab
+          color="primary"
+          aria-label="add"
+          onClick={() => console.log("Add Exercise Clicked")}
+        >
+          <AddIcon />
+        </Fab>
+      </div>
     </main>
   );
 }
