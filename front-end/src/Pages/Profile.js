66bdf84ad655b307ed65c3ef99ca8b7f1c2bb3de
Jack Robinson <jack.robinson@ibm.com>
2022-04-03 18:04:17 +0100

    test commit

--- front-end/src/Pages/Profile.js
+++ front-end/src/Pages/Profile.js
@@ -3,7 +3,7 @@ import { React } from "react";
 export default function Profile() {
   return (
     <main style={{ padding: "20px" }}>
-      <h2>Profile</h2>
+      <h2>Profile Test</h2>
       This will be where the person's profile is
     </main>
   );
