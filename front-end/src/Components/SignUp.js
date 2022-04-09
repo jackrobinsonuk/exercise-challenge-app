469763f77aeba2e6f53e87824a3ba82423643489
Jack Robinson <jack.robinson@ibm.com>
2022-04-09 15:32:16 +0100

    updated sign up regex

--- front-end/src/Components/SignUp.js
+++ front-end/src/Components/SignUp.js
@@ -14,17 +14,77 @@ export default function SignUp(props) {
   const [textFieldErrorState, setTextFieldErrorState] = useState(false);
   const [error, setError] = useState("");
   const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [emailAddressFieldWarning, setEmailAddressFieldWarning] = useState("");
+  const [passwordFieldWarning, setPasswordFieldWarning] = useState("");
+  const [confirmPasswordFieldWarning, setConfirmPasswordFieldWarning] =
+    useState("");
+  const [warning, setWarning] = useState("");
+
+  const validateEmailAddress = (event) => {
+    return String(event)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const analyzeEmailAddress = (event) => {
+    if (validateEmailAddress(event.target.value)) {
+      setEmailAddressFieldWarning("");
+      setWarning("");
+    } else {
+      setEmailAddressFieldWarning("warning");
+      setWarning("Please check your email address.");
+    }
+  };
+
+  const analyzePassword = (event) => {
+    const strongRegex = new RegExp(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+    );
+
+    if (strongRegex.test(event.target.value)) {
+      setPasswordFieldWarning("");
+      setWarning("");
+    } else {
+      setPasswordFieldWarning("warning");
+      setWarning(
+        "Your password should contain: 1 Uppercase, 1 Lowercase, 1 Number, 1 Symbol"
+      );
+    }
+  };
+
+  const analyzeConfirmPassword = (event) => {
+    const strongRegex = new RegExp(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+    );
+    if (strongRegex.test(event.target.value)) {
+      setPasswordFieldWarning("");
+      setWarning("");
+    } else if (confirmPassword !== password) {
+      setConfirmPasswordFieldWarning("warning");
+      setWarning("Your passwords do not match.");
+    } else {
+      setPasswordFieldWarning("warning");
+      setWarning(
+        "Your password should contain: 1 Uppercase, 1 Lowercase, 1 Number, 1 Symbol"
+      );
+    }
+  };
 
   const handleEmailAddressChange = (event) => {
     setEmailAddress(event.target.value);
+    analyzeEmailAddress(event);
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
+    analyzePassword(event);
   };
 
   const handleConfirmPasswordChange = (event) => {
     setConfirmPassword(event.target.value);
+    analyzeConfirmPassword(event);
   };
 
   const handleLoginClick = () => {
@@ -76,6 +136,7 @@ export default function SignUp(props) {
         <FormControl fullWidth>
           <TextField
             error={textFieldErrorState}
+            color={emailAddressFieldWarning}
             required
             id="outlined-basic"
             label="Email Address"
@@ -87,6 +148,7 @@ export default function SignUp(props) {
 
           <TextField
             error={textFieldErrorState}
+            color={passwordFieldWarning}
             fullWidth
             required
             id="outlined-password-input"
@@ -99,6 +161,7 @@ export default function SignUp(props) {
           />
           <TextField
             error={textFieldErrorState}
+            color={confirmPasswordFieldWarning}
             fullWidth
             required
             id="outlined-password-input"
@@ -140,6 +203,12 @@ export default function SignUp(props) {
             <b>{props.error.toString()}</b>
             <b>{error}</b>
           </div>
+          {warning && (
+            <div>
+              <b>Warning: </b>
+              {warning}
+            </div>
+          )}
         </FormControl>
       </Box>
     </main>
