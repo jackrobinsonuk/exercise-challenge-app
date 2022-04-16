2a985efc20629481ea747ec3f8ccda6b73ff24fc
Jack Robinson <jack.robinson@ibm.com>
2022-04-16 13:56:47 +0100

    updated notes in generate-leagues

--- back-end/src/WorkerCluster/generate-leagues.js
+++ back-end/src/WorkerCluster/generate-leagues.js
@@ -13,3 +13,6 @@
 // the last user in the rank is subtracted 1 tier points and demoted
 
 // the new league data should be saved to the s3 bucket to allow it to be fetched
+
+// Sample League Data (this is what the FE expects) can be found in the FE portion of the repo
+// If the service contract cannot be achieved, then the FE UI will need to be redesigned
