b122be4ccf169988291b052d0fe81f708a7a9d20
Jack Robinson <jack.robinson@ibm.com>
2022-04-16 13:53:29 +0100

    created outline for how to generate the league data

---
+++ back-end/src/UserCluster/get-leagues.js
@@ -0,0 +1,45 @@
+// fetch data from the S3 bucket with the league data in
+// return it to the user
+// query by week number
+
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+const bucketName = process.env.LEAGUES_BUCKET;
+
+exports.getLeaguesHandler = async (event, context, callback) => {
+  const s3Bucket = bucketName;
+  const weekNumber = event.queryStringParameters.weekNumber;
+
+  try {
+    const params = {
+      Bucket: s3Bucket,
+      Key: weekNumber,
+    };
+    const result = await s3.getObject(params).promise();
+
+    const response = {
+      statusCode: 200,
+      body: result.Body.toString("utf-8"),
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*", // Allow from anywhere
+        "Access-Control-Allow-Methods": "GET", // Allow only POST & OPTIONS request
+      },
+    };
+    console.log(response.body);
+    return response;
+  } catch (error) {
+    console.log(error);
+    const response = {
+      statusCode: 404,
+      body: JSON.stringify(error),
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*", // Allow from anywhere
+        "Access-Control-Allow-Methods": "GET", // Allow only POST & OPTIONS request
+      },
+    };
+    return response;
+  }
+};
---
+++ back-end/src/WorkerCluster/generate-leagues.js
@@ -0,0 +1,15 @@
+// This lambda function will update the leagues S3 bucket with the data from the latest week
+
+// Note: this may need doing over multiple functions and use StepFunctons to chain it together
+
+// Find out what week number it is in the challenge
+// Get all exercise completed in the week
+// Get current league data
+// Update current league data with new data from the week
+// Re-rank the users based on the league data, ordering by points
+
+// the first user in the rank is added 2 tier points and promoted
+// the second user in the rank is added 1 tier point
+// the last user in the rank is subtracted 1 tier points and demoted
+
+// the new league data should be saved to the s3 bucket to allow it to be fetched
