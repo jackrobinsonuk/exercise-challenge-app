806a896ec73347c58b193ee37fae8233331eaf66
Jack <robinson.jack97@gmail.com>
2022-08-15 17:03:34 +0100

    create get-all-data api

---
+++ back-end/src/AdminCluster/get-all-data.js
@@ -0,0 +1,24 @@
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient({ region: "ap-southeast-1" });
+
+const params = {
+  TableName: process.env.USER_EXERCISE_TABLE,
+};
+
+async function listItems() {
+  try {
+    const data = await docClient.scan(params).promise();
+    return data;
+  } catch (err) {
+    return err;
+  }
+}
+
+exports.getAllDataHandler = async (event, context) => {
+  try {
+    const data = await listItems();
+    return { body: JSON.stringify(data) };
+  } catch (err) {
+    return { error: err };
+  }
+};
